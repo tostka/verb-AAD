a3a6a3486555d3e907909b145c4a0de31ea6f930
Tostka <tostka@users.noreply.github.com>
2019-12-10 22:05:35 -0600

    interrum pass on a build, debugging
    *10:05 PM 12/10/2019

--- verb-AAD/verb-AAD.psm1
+++ verb-AAD/verb-AAD.psm1
@@ -4,7 +4,7 @@
   .SYNOPSIS
   verb-AAD - Azure AD-related generic functions
   .NOTES
-  Version     : 0.0.2.1
+  Version     : 0.0.0.1
   Author      : Todd Kadrie
   Website     :	https://www.toddomation.com
   Twitter     :	@tostka
@@ -17,23 +17,48 @@
   AddedWebsite:	REFERENCEURL
   AddedTwitter:	@HANDLE / http://twitter.com/HANDLE
   REVISIONS
-  * 12/10/2019 - 0.0.2.1
-  * 10:55 AM 12/6/2019 Connect-MSOL & Connect-AAD:added suffix to TitleBar tag for non-TOR tenants, also config'd a central tab vari
-* 1:07 PM 11/25/2019 added *tol/*tor/*cmw alias variants for connect & reconnect
-* 9:19 AM 11/19/2019 added MFA tenant detect (fr cred), and code to support MFA, splits specified credential and picks up on global o365_TAG_MFA/o365_TAG_OPDomain varis matching the credential domain. also added Add-PSTitleBar 'XXX' for msol & aad ;
-* 2:18 PM 5/14/2019 added Build-AADSignErrorsHash 
-* 2:53 PM 5/2/2019 ren'd Connect-AAD2 -> Connect-AAD ; ren'd Connect-AAD -> Connect-MSOL ; repurp'ing connect-aad for AzureAD module
-* 11:56 AM 12/7/2018 init version, added Alias connect-msol -> connect-aad
+  * 12/10/2019 - 0.0.0.1
   .DESCRIPTION
   verb-AAD - Azure AD-related generic functions
+  .PARAMETER  PARAMNAME
+  PARAMDESC
+  .PARAMETER  Mbx
+  Mailbox identifier [samaccountname,name,emailaddr,alias]
+  .PARAMETER  Computer
+  Computer Name [-ComputerName server]
+  .PARAMETER  ServerFqdn
+  Server Fqdn (24-25char) [-serverFqdn lynms650.global.ad.toro.com)] 
+  .PARAMETER  Server
+  Server NBname (8-9chars) [-server lynms650)]
+  .PARAMETER  SiteName
+  Specify Site to analyze [-SiteName (USEA|GBMK|AUSYD]
+  .PARAMETER  Ticket
+  Ticket # [-Ticket nnnnn]
+  .PARAMETER  Path
+  Path [-path c:\path-to\]
+  .PARAMETER  File
+  File [-file c:\path-to\file.ext]
+  .PARAMETER  String
+  2-30 char string [-string 'word']
+  .PARAMETER  Credential
+  Credential (PSCredential obj) [-credential ]
+  .PARAMETER  Logonly
+  Run a Test no-change pass, and log results [-Logonly]
+  .PARAMETER  FORCEALLPINS
+  Reset All PINs (boolean) [-FORCEALLPINS:True]
+  .PARAMETER Whatif
+  Parameter to run a Test no-change pass, and log results [-Whatif switch]
+  .PARAMETER ShowProgress
+  Parameter to display progress meter [-ShowProgress switch]
+  .PARAMETER ShowDebug
+  Parameter to display Debugging messages [-ShowDebug switch]
   .INPUTS
   None
   .OUTPUTS
   None
   .EXAMPLE
-  import-module verb-AAD
+  .EXAMPLE
   .LINK
-  https://github.com/tostka/verb-AAD
   #>
 
 
@@ -52,29 +77,3 @@ ForEach ($folder in $functionFolders) {
 } ;
 $publicFunctions = (Get-ChildItem -Path "$PSScriptRoot\Public" -Filter '*.ps1').BaseName ;
 Export-ModuleMember -Function $publicFunctions ;
-
-# SIG # Begin signature block
-# MIIELgYJKoZIhvcNAQcCoIIEHzCCBBsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU4feJNcsQ6O6QTfuyl0INcdJ6
-# koegggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
-# MCwxKjAoBgNVBAMTIVBvd2VyU2hlbGwgTG9jYWwgQ2VydGlmaWNhdGUgUm9vdDAe
-# Fw0xNDEyMjkxNzA3MzNaFw0zOTEyMzEyMzU5NTlaMBUxEzARBgNVBAMTClRvZGRT
-# ZWxmSUkwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALqRVt7uNweTkZZ+16QG
-# a+NnFYNRPPa8Bnm071ohGe27jNWKPVUbDfd0OY2sqCBQCEFVb5pqcIECRRnlhN5H
-# +EEJmm2x9AU0uS7IHxHeUo8fkW4vm49adkat5gAoOZOwbuNntBOAJy9LCyNs4F1I
-# KKphP3TyDwe8XqsEVwB2m9FPAgMBAAGjdjB0MBMGA1UdJQQMMAoGCCsGAQUFBwMD
-# MF0GA1UdAQRWMFSAEL95r+Rh65kgqZl+tgchMuKhLjAsMSowKAYDVQQDEyFQb3dl
-# clNoZWxsIExvY2FsIENlcnRpZmljYXRlIFJvb3SCEGwiXbeZNci7Rxiz/r43gVsw
-# CQYFKw4DAh0FAAOBgQB6ECSnXHUs7/bCr6Z556K6IDJNWsccjcV89fHA/zKMX0w0
-# 6NefCtxas/QHUA9mS87HRHLzKjFqweA3BnQ5lr5mPDlho8U90Nvtpj58G9I5SPUg
-# CspNr5jEHOL5EdJFBIv3zI2jQ8TPbFGC0Cz72+4oYzSxWpftNX41MmEsZkMaADGC
-# AWAwggFcAgEBMEAwLDEqMCgGA1UEAxMhUG93ZXJTaGVsbCBMb2NhbCBDZXJ0aWZp
-# Y2F0ZSBSb290AhBaydK0VS5IhU1Hy6E1KUTpMAkGBSsOAwIaBQCgeDAYBgorBgEE
-# AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
-# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBQjGEwC
-# KHcGIqve0D7dpxhnN6PnLTANBgkqhkiG9w0BAQEFAASBgJBAR+jw6xD1uqLg8kMW
-# 1L2bRoKpp8/FmhhYJAQQyzLtkPfpYFdssRbRaURkoVmDflLDqyi3u6p/2hgg8Of7
-# CDPOBGGQXxRvrxYtW+PrSeiLA+ZROArq9ypTVXpFKf38w75IUzbaEaVrOHXcuuQg
-# 712pWEh7i3C6kx9W5wuKvb8R
-# SIG # End signature block
