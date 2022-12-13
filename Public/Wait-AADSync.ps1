14e0050819e17081bbe058bd96b2b9f9ac345b09
Tostka <tostka@users.noreply.github.com>
2022-12-13 14:24:57 -0600

    Update Wait-AADSync.ps1
     * 2:05 PM 12/13/2022 recoded for AzureAD backend (with msol deprecated; shouldn't have used aad in the name, initially, with msol as the backend).

--- Public/Wait-AADSync.ps1
+++ Public/Wait-AADSync.ps1
@@ -16,6 +16,7 @@ Function Wait-AADSync {
     Tags        : Powershell
     Updated By: : Todd Kadrie
     REVISIONS   :
+    * 2:05 PM 12/13/2022 recoded for AzureAD backend (with msol deprecated; shouldn't have used aad in the name, initially, with msol as the backend).
     * 4:22 PM 7/24/2020 added verbose
     * 12:14 PM 5/27/2020 moved alias:wait-msolsync win the func
     * 10:27 AM 2/25/2020 bumped polling interval to 30s
@@ -38,6 +39,7 @@ Function Wait-AADSync {
     [Alias('Wait-MSolSync')]
     Param([Parameter()]$Credential = $global:credo365TORSID) ;
     $verbose = ($VerbosePreference -eq "Continue") ; 
+    <# MSOL original
     try { Get-MsolAccountSku -ErrorAction Stop | out-null }
     catch [Microsoft.Online.Administration.Automation.MicrosoftOnlineException] {
         "Not connected to MSOnline. Now connecting." ;
@@ -48,6 +50,26 @@ Function Wait-AADSync {
     Do { Connect-AAD  ; write-host "." -NoNewLine ; Start-Sleep -m (1000 * 30) ; Connect-MSOL } Until ((Get-MsolCompanyInformation).LastDirSyncTime -ne $DirSyncLast) ;
     write-host -foregroundcolor yellow "]`n$((get-date).ToString('HH:mm:ss')):AD->AAD REPLICATED!" ;
     write-host "`a" ; write-host "`a" ; write-host "`a" ;
+    #>
+    
+    try { $AADTenDtl = Get-AzureADTenantDetail -ErrorAction Stop } # authenticated to "a" tenant
+    catch { 
+        write-host "(Not connected to AzureAD. Now connecting)" ;
+        Connect-AAD ;
+        $AADTenDtl = Get-AzureADTenantDetail -ErrorAction Stop ; 
+    } ;
+    $DirSyncLast = $AADTenDtl.CompanyLastDirSyncTime ; 
+    write-host -foregroundcolor yellow "$((get-date).ToString('HH:mm:ss')):Waiting for next AAD Dirsync:`n(prior:$($DirSyncLast.ToLocalTime()))`n[" ;
+
+    Do { 
+        Connect-AAD -silent  ;
+        write-host "." -NoNewLine ;
+        Start-Sleep -m (1000 * 30) ;
+    } Until ((Get-AzureADTenantDetail).CompanyLastDirSyncTime -ne $DirSyncLast) ;
+    write-host -foregroundcolor yellow "]`n$((get-date).ToString('HH:mm:ss')):AD->AAD REPLICATED!" ;
+    write-host "`a" ;
+    write-host "`a" ;
+    write-host "`a" ;
 }
 
 #*------^ Wait-AADSync.ps1 ^------
