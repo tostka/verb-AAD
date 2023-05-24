d5ecee009114853a1e543c3efcf08126480751fe
Tostka <tostka@users.noreply.github.com>
2023-05-24 16:32:29 -0500

    Update set-AADUserUsageLocation.ps1
    * 3:52 PM 5/23/2023 implemented @rxo @rxoc split, (silence all connectivity, non-silent feedback of functions); flipped all r|cxo to @pltrxoC, and left all function calls as @pltrxo;
        * 12:01 PM 5/22/2023 add: missing w-o for sucess on report; also test $aad.usageloc actually updated; updated cbh ;
        * 9:55 AM 5/19/2023 CBH, added full call example context
        * 1:44 PM 5/17/2023 rounded out params for $pltRXO passthru

--- Public/set-AADUserUsageLocation.ps1
+++ Public/set-AADUserUsageLocation.ps1
@@ -20,6 +20,10 @@ function set-AADUserUsageLocation {
     AddedWebsite:	
     AddedTwitter:	URL
     REVISIONS
+    * 3:52 PM 5/23/2023 implemented @rxo @rxoc split, (silence all connectivity, non-silent feedback of functions); flipped all r|cxo to @pltrxoC, and left all function calls as @pltrxo; 
+    * 12:01 PM 5/22/2023 add: missing w-o for sucess on report; also test $aad.usageloc actually updated; updated cbh ; 
+    * 9:55 AM 5/19/2023 CBH, added full call example context
+    * 1:44 PM 5/17/2023 rounded out params for $pltRXO passthru
     * 10:30 AM 3/24/2022 add pipeline support
     2:31 PM 3/22/2022 init, simple subset port of set-aaduserLicense() ; 
     .DESCRIPTION
@@ -36,6 +40,38 @@ function set-AADUserUsageLocation {
     PS> $bRet = set-AADUserUsageLocation -users 'upn@domain.com','upn2@domain.com' -usageLocation 'US' -verbose ;
     PS> $bRet | %{if($_.Success){write-host "$($_.AzureADUser.userprincipalname):Success"} else { write-warning "$($_.AzureADUser.userprincipalname):FAILURE" } ; 
     Add US UsageLocation to the array of user UPNs specified in -users, with verbose output
+    .EXAMPLE
+    PS>  if (-not $AADUser.UsageLocation) {
+    PS>      $smsg = "AADUser: MISSING USAGELOCATION, FORCING" ;
+    PS>      if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN } #Error|Warn|Debug
+    PS>      else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
+    PS>      $spltSAADUUL = [ordered]@{
+    PS>          Users = $AADUser.UserPrincipalName ;
+    PS>          UsageLocation = "US" ;
+    PS>          whatif = $($whatif) ;
+    PS>          Credential = $pltRXO.Credential ;
+    PS>          verbose = $pltRXO.verbose  ;
+    PS>          silent = $false ;
+    PS>      } ;
+    PS>      $smsg = "set-AADUserUsageLocationw`n$(($spltSAADUUL|out-string).trim())" ;
+    PS>      if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info }
+    PS>      else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
+    PS>      $bRet = set-AADUserUsageLocation @spltSAADUUL ;
+    PS>      if($bRet.Success){
+    PS>          $smsg = "set-AADUserUsageLocation updated UsageLocation:$($bRet.AzureADuser.UsageLocation)" ;
+    PS>          if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info }
+    PS>          else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
+    PS>          $AADUser = $bRet.AzureADuser ;
+    PS>      } else {
+    PS>          $smsg = "set-AADUserUsageLocation: FAILED TO UPDATE USAGELOCATION!" ;
+    PS>          if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level WARN } #Error|Warn|Debug
+    PS>          else{ write-WARNING "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
+    PS>          if(-not $whatif){
+    PS>              BREAK;
+    PS>          }
+    PS>      } ;
+    PS>  } ;
+    Fully rounded out call example, with post testing. 
     .LINK
     https://github.com/tostka/verb-AAD
     #>
@@ -46,29 +82,38 @@ function set-AADUserUsageLocation {
     [CmdletBinding()]
     PARAM (
         # ValueFromPipeline: will cause params to match on matching type, [array] input -> [array]$param
-        [Parameter(Mandatory=$false,ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$false,ValueFromPipeline=$true,HelpMessage="User Identifiers[-Users 'UPN']")]
         [ValidateNotNullOrEmpty()]
-        [string[]]$Users, 
-        [string]$UsageLocation = 'US',
+            [string[]]$Users, 
+        [Parameter(Mandatory=$false,HelpMessage="Microsoft UsageLocation code[-UsageLocation 'US']")]
+            [string]$UsageLocation = 'US',
         [Parameter(Mandatory=$false,HelpMessage="Tenant Tag to be processed[-PARAM 'TEN1']")]
-        [ValidateNotNullOrEmpty()]
-        [string]$TenOrg = $global:o365_TenOrgDefault,
-        [Parameter(Mandatory=$False,HelpMessage="Credentials [-Credentials [credential object]]")]
-        [System.Management.Automation.PSCredential]$Credential = $global:credo365TORSID,
-        [switch]$whatif,
-        [switch]$silent
+            [ValidateNotNullOrEmpty()]
+            [string]$TenOrg = $global:o365_TenOrgDefault,
+        [Parameter(Mandatory = $false, HelpMessage = "Use specific Credentials (defaults to Tenant-defined SvcAccount)[-Credentials [credential object]]")]
+            [System.Management.Automation.PSCredential]$Credential,
+        [Parameter(HelpMessage="Silent output (suppress status echos)[-silent]")]
+            [switch] $silent,
+        [Parameter(HelpMessage="Whatif Flag  [-whatIf]")]
+            [switch] $whatIf
     ) ;
     BEGIN {
         ${CmdletName} = $PSCmdlet.MyInvocation.MyCommand.Name ;
         $Verbose = ($VerbosePreference -eq 'Continue') ;
        
-       $pltRXO = [ordered]@{
-            Credential = $Credential 
-            verbose = $($VerbosePreference -eq 'Continue') ;
-            silent = $true ; # always silent, echo only warn/errors
-        } ; 
+        # downstream commands
+        $pltRXO = [ordered]@{
+            Credential = $Credential ;
+            verbose = $($VerbosePreference -eq "Continue")  ;
+        } ;
+        if((gcm Reconnect-EXO).Parameters.keys -contains 'silent'){
+            $pltRxo.add('Silent',$silent) ;
+        } ;
+        # default connectivity cmds - force silent false
+        $pltRXOC = [ordered]@{} ; $pltRXO.GetEnumerator() | ?{ $_.Key -notmatch 'silent' }  | ForEach-Object { $pltRXOC.Add($_.Key, $_.Value) } ; $pltRXOC.Add('silent',$true) ;
+        if((gcm Reconnect-EXO).Parameters.keys -notcontains 'silent'){ $pltRxo.remove('Silent') } ; 
         #Connect-AAD -Credential:$Credential -verbose:$($verbose) ;
-        Connect-AAD @pltRXO ;         
+        Connect-AAD @pltRXOC ;         
         
         # check if using Pipeline input or explicit params:
         if ($PSCmdlet.MyInvocation.ExpectingInput) {
@@ -98,25 +143,6 @@ function set-AADUserUsageLocation {
             $error.clear() ;
             TRY {
 
-                <# rem out search string option - if we're mandating UPN/guids, it's always going to fail the initial attempt, skip it, odds of feeding it a dname etc is low
-                $pltGAADU=[ordered]@{ SearchString = $user ; ErrorAction = 'STOP' ; verbose = ($VerbosePreference -eq "Continue") ; } ; 
-                $smsg = "Get-AzureADUser w`n$(($pltGAADU|out-string).trim())" ; 
-                if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
-                else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;                      
-                $AADUser = Get-AzureADUser @pltGAADU ; 
-                if (-not $AADUser) {
-                
-                    $smsg = "Failed: Get-AzureADUser -SearchString $($pltGAADU.searchstring)" ; 
-                    $smsg += "`nretrying as -objectid..." ; 
-                    if($silent){} elseif ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
-                    else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;            
-                    $pltGAADU.remove('SearchString') ; 
-                    $pltGAADU.Add("ObjectID",$user) ; 
-                    if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
-                    else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;                      
-                    $AADUser = Get-AzureADUser @pltGAADU ;         
-                } ; 
-                #>
                 $pltGAADU=[ordered]@{ ObjectID = $user ; ErrorAction = 'STOP' ; verbose = ($VerbosePreference -eq "Continue") ; } ; 
                 $smsg = "Get-AzureADUser w`n$(($pltGAADU|out-string).trim())" ; 
                 if($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
@@ -148,13 +174,20 @@ function set-AADUserUsageLocation {
                                     $smsg = "POST:Confirming UsageLocation -eq US:$($AADUser.UsageLocation)" ; 
                                     if($silent){} elseif ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } #Error|Warn|Debug 
                                     else{ write-host -foregroundcolor green "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ;
-                                    
-                                    $Report = @{
-                                        AzureADUser = $AADUser ; 
-                                        FixedUsageLocation = $true ; 
-                                        Success = $true ; 
-                                    } ; 
-
+                                    if($AADUser.UsageLocation -eq $spltSAADU.UsageLocation){
+                                        $Report = @{
+                                            AzureADUser = $AADUser ; 
+                                            FixedUsageLocation = $true ; 
+                                            Success = $true ; 
+                                        } ; 
+                                    } else { 
+                                         $Report = @{
+                                            AzureADUser = $AADUser ; 
+                                            FixedUsageLocation = $false ; 
+                                            Success = $false ; 
+                                        } ; 
+                                    } ;
+                                    $Report | write-output ; 
                                 } CATCH {
                                   $ErrTrapd=$_ ;
                                   $smsg = "$('*'*5)`nFailed processing $($ErrTrapd.Exception.ItemName). `nError Message: $($ErrTrapd.Exception.Message)`nError Details: `n$(($ErrTrapd|out-string).trim())`n$('-'*5)" ;
@@ -218,3 +251,4 @@ function set-AADUserUsageLocation {
         else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;         
     } ;
 } ; 
+#*------^ END Function set-AADUserUsageLocation ^------
