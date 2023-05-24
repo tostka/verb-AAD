c8ecd565ff28393ea7e33b485e8590589ee65ef2
Tostka <tostka@users.noreply.github.com>
2023-05-24 16:31:43 -0500

    Update get-AADToken.ps1
    * 4:21 PM 5/22/2023 added -silent, and pswlt support;
        * 3:29 PM 5/10/2023 tweaked verbose comments re: token status
        * 12:59 PM 5/9/2023 added trailing test for unauth, single tenant auth, and multi-token auth.

--- Public/get-AADToken.ps1
+++ Public/get-AADToken.ps1
@@ -1,4 +1,6 @@
-﻿#*------v get-AADToken.ps1 v------
+﻿# get-AADToken.ps1
+
+#*------v get-AADToken.ps1 v------
 function get-AADToken {
     <#
     .SYNOPSIS
@@ -14,10 +16,14 @@ function get-AADToken {
     Copyright   : (c) 2020 Todd Kadrie
     Github      : https://github.com/tostka/verb-aad
     REVISIONS
+    * 4:21 PM 5/22/2023 added -silent, and pswlt support; 
+    * 3:29 PM 5/10/2023 tweaked verbose comments re: token status
+    * 12:59 PM 5/9/2023 added trailing test for unauth, single tenant auth, and multi-token auth.
     * 8:50 AM 3/16/2021 added extra catchblock on expired token, but found that MS had massive concurrent Auth issues, so didn't finish - isolated event, not a normal fail case
     * 12:21 PM 8/8/2020 init
     .DESCRIPTION
     get-AADToken - Retrieve and summarize [Microsoft.Open.Azure.AD.CommonLibrary.AzureSession]::AccessTokens
+    Works with MSAL (as it's accessing the underlying class).
     .EXAMPLE
     $token = get-AADToken ; 
     if( ($null -eq $token) -OR ($token.count -eq 0)){
@@ -31,7 +37,10 @@ function get-AADToken {
     https://github.com/tostka/verb-aad
     #>
     [CmdletBinding()] 
-    Param([Parameter()][System.Management.Automation.PSCredential]$Credential = $global:credo365TORSID) ;
+    PARAM(
+        [Parameter(HelpMessage="Silent output (suppress status echos)[-silent]")]
+            [switch] $silent
+    ) ;
     BEGIN {$verbose = ($VerbosePreference -eq "Continue") } ;
     PROCESS {
         $token = $false ;
@@ -49,7 +58,9 @@ function get-AADToken {
                     + CategoryInfo          : InvalidOperation: (Microsoft.Open....ry.AzureSession:TypeName) [], RuntimeException
                     + FullyQualifiedErrorId : TypeNotFound
             #>
-            write-verbose "(No authenticated connection found)"
+            $smsg = "(No authenticated connection found)" ;
+            if($silent){}elseif($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
             #$token = $false ; 
         } CATCH [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException] {
             # reflects unauthenticated
@@ -60,17 +71,34 @@ function get-AADToken {
                 + CategoryInfo          : NotSpecified: (:) [Get-AzureADTenantDetail], AadNeedAuthenticationException
                 + FullyQualifiedErrorId : Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException,Microsoft.Open.AzureAD16.PowerShell.GetTenantDetails 
             #>
-            write-verbose "(requires AAD authentication)"
+            $smsg = "(requires AAD authentication)" ;
+            if($silent){}elseif($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
         } CATCH {
             Write-Warning "$(get-date -format 'HH:mm:ss'): Failed processing $($_.Exception.ItemName). `nError Message: $($_.Exception.Message)`nError Details: $($_)" ;
             Exit #Opts: STOP(debug)|EXIT(close)|CONTINUE(move on in loop cycle)|BREAK(exit loop iteration)|THROW $_/'CustomMsg'(end script with Err output)
         } ;  
     } ; 
     END{ 
-        if($token.count -gt 1){
-            write-verbose "(returning $(($token|measure).count) tokens)" ; 
+        if( ($null -eq $token) -OR ($token.count -eq 0)){
+            $smsg = "no token: unconnected" ; 
+            if($silent){}elseif($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
+        }elseif($token.count -eq 1){
+            $smsg = "(returning $(($token|measure).count) token)" ; 
+            if($silent){}elseif($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
+            
+        }elseif($token.count -gt 1){
+            $smsg = "(returning $(($token|measure).count) tokens)" ; 
+            if($silent){}elseif($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
         } ; 
-        write-verbose "(Connected to tenant: $($token.AccessToken.TenantId) with user: $($token.AccessToken.UserId)" ; 
+        if($token.count -gt 0){
+            $smsg = "(Connected to tenant: $($token.AccessToken.TenantId) with user: $($token.AccessToken.UserId)" ; 
+            if($silent){}elseif($verbose){if ($logging) { Write-Log -LogContent $smsg -Path $logfile -useHost -Level Info } 
+            else{ write-verbose "$((get-date).ToString('HH:mm:ss')):$($smsg)" } ; } ;
+        } ; 
         $token | Write-Output 
     } ;
 }
